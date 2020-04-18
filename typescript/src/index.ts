0b80a1840d87620495ecc5ba792f0d5a18b36305
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-04-18 17:29:13 -0300

    Adicionar tipos

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,10 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import { helloWorld } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-})
+app.get('/', helloWorld);
 
 app.listen(3333);
 
